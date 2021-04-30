9e542ad66ac59cb00bdbf6dc603810bdce09b7c8
Juthika Paul <57178572+juthipaul@users.noreply.github.com>
2021-04-30 16:08:34 +0530

    Update paths.js

--- resources/paths.js
+++ resources/paths.js
@@ -1,8 +1,6 @@
 function pathURLs()
 {
-    this.contentReadPath="content/v1/read/do_31321401593029427218";
-    this.userCreatePath="/api/user/v1/signup";
-    this.userRead="/api/user/v2/read/4f83a9d9-f063-48c1-a28a-687a21f5bc0b";
+
     this.tokenGeneration="/auth/realms/sunbird/protocol/openid-connect/token";
     this.programCreate="/api/program/v1/create";
     this.programRead="/content/program/v1/read/";
@@ -10,7 +8,6 @@ function pathURLs()
     this.getListCategories="/discussion/categories";
     this.fetchNodebbUserDetails="/discussion/user/adarshr_l1uq";
     this.createNewGroup="/learner/group/v1/create";
-    //this.joinGroup="";
     this.getForumId="/discussion/forum/v2/read";
 }
 
